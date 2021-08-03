59c475f0048f3ff9f15b178824252ba6e706993b
cobrex1 <44735826+cobrex1@users.noreply.github.com>
2021-08-03 15:51:40 +0100

    1)Updated so Jobs and mcMMO are now soft depend. 2) Added check for if existing booster is running.

--- src/main/java/com/tamrielnetwork/gbooster/gbooster/GBooster.java
+++ src/main/java/com/tamrielnetwork/gbooster/gbooster/GBooster.java
@@ -4,9 +4,10 @@ import com.tamrielnetwork.gbooster.gbooster.bar.BoosterBar;
 import com.tamrielnetwork.gbooster.gbooster.commands.GBoosterCmd;
 import com.tamrielnetwork.gbooster.gbooster.files.Messages;
 import com.tamrielnetwork.gbooster.gbooster.listeners.*;
-import com.tamrielnetwork.gbooster.gbooster.storage.*;
 import com.tamrielnetwork.gbooster.gbooster.managers.BoostersManager;
 import com.tamrielnetwork.gbooster.gbooster.papi.BoosterExpansion;
+import com.tamrielnetwork.gbooster.gbooster.storage.*;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -15,46 +16,46 @@ import java.util.Objects;
 
 public final class GBooster extends JavaPlugin {
 
-    private BoostersManager boostersManager;
-    private BoostersStorage boostersStorage;
-    private PlayerStorage playerStorage;
-    private BoosterBar boosterBar;
-    private Messages messages;
+	private BoostersManager boostersManager;
+	private BoostersStorage boostersStorage;
+	private PlayerStorage playerStorage;
+	private BoosterBar boosterBar;
+	private Messages messages;
 
-    @Override
-    public void onEnable() {
-        System.out.println("""
-                [GBooster] GBooster Copyright (C) 2021 Leopold Meinel
-                [GBooster] This program comes with ABSOLUTELY NO WARRANTY!
-                [GBooster] This is free software, and you are welcome to redistribute it under certain conditions.
-                [GBooster] Visit https://github.com/TamrielNetwork/GBooster/blob/main/LICENSE for more details.""");
+	@Override
+	public void onEnable() {
+		System.out.println("""
+				[GBooster] GBooster Copyright (C) 2021 Leopold Meinel
+				[GBooster] This program comes with ABSOLUTELY NO WARRANTY!
+				[GBooster] This is free software, and you are welcome to redistribute it under certain conditions.
+				[GBooster] Visit https://github.com/TamrielNetwork/GBooster/blob/main/LICENSE for more details.""");
 
-        generateConfig();
+		generateConfig();
 
-        registerListeners();
+		registerListeners();
 
-        Objects.requireNonNull(getCommand("gbooster")).setExecutor(new GBoosterCmd());
+		Objects.requireNonNull(getCommand("gbooster")).setExecutor(new GBoosterCmd());
 
-        setupStorage();
-        boostersManager = new BoostersManager();
-        boostersManager.loadBoosters();
-        playerStorage.loadPlayers();
-        boostersStorage.loadBoosters();
+		setupStorage();
+		boostersManager = new BoostersManager();
+		boostersManager.loadBoosters();
+		playerStorage.loadPlayers();
+		boostersStorage.loadBoosters();
 
-        boosterBar = new BoosterBar();
-        messages = new Messages();
+		boosterBar = new BoosterBar();
+		messages = new Messages();
 
-        new BoosterExpansion().register();
+		new BoosterExpansion().register();
 
-        startSaveTask();
-    }
+		startSaveTask();
+	}
 
-    @Override
-    public void onDisable(){
-        playerStorage.savePlayers();
-        boostersStorage.saveBoosters();
-    }
-
+	@Override
+	public void onDisable() {
+		playerStorage.savePlayers();
+		boostersStorage.saveBoosters();
+	}
+/*
     private void registerListeners(){
         getServer().getPluginManager().registerEvents(new JobsExpGain(), this);
         getServer().getPluginManager().registerEvents(new JobsPayment(), this);
@@ -64,53 +65,74 @@ public final class GBooster extends JavaPlugin {
         getServer().getPluginManager().registerEvents(new PlayerExpChange(), this);
         getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
     }
+*/
 
-    private void generateConfig(){
-        File config = new File(getDataFolder(), "config.yml");
-        if (!config.exists()) {
-            saveDefaultConfig();
-        }
-    }
+	private void registerListeners() {
+		if (getServer().getPluginManager().getPlugin("Jobs") != null) {
+			Bukkit.getLogger().info("Found Jobs! - Registering listeners!");
+			getServer().getPluginManager().registerEvents(new JobsExpGain(), this);
+			getServer().getPluginManager().registerEvents(new JobsPayment(), this);
+			getServer().getPluginManager().registerEvents(new JobsScheduleStart(), this);
+			getServer().getPluginManager().registerEvents(new JobsScheduleStop(), this);
+		} else {
+			Bukkit.getLogger().info("Could not find Jobs - Ignoring.");
+		}
+		if (getServer().getPluginManager().getPlugin("mcMMO") != null) {
+			Bukkit.getLogger().info("Found McMMO! - Registering listener!");
+			getServer().getPluginManager().registerEvents(new McMMOPlayerXpGain(), this);
+		} else {
+			Bukkit.getLogger().info("Could not find McMMO - Ignoring.");
+		}
+		getServer().getPluginManager().registerEvents(new PlayerExpChange(), this);
+		getServer().getPluginManager().registerEvents(new PlayerJoin(), this);
+	}
 
-    private void setupStorage(){
-        String storageSystem = getConfig().getString("storage-system");
+	private void generateConfig() {
+		File config = new File(getDataFolder(), "config.yml");
+		if (!config.exists()) {
+			saveDefaultConfig();
+		}
+	}
 
-        if (Objects.requireNonNull(storageSystem).equalsIgnoreCase("mysql")){
-            this.playerStorage = new PlayerStorageSql();
-            this.boostersStorage = new BoosterStorageSql();
-        } else {
-            this.playerStorage = new PlayerStorageYaml();
-            this.boostersStorage = new BoosterStorageYaml();
-        }
-    }
+	private void setupStorage() {
+		String storageSystem = getConfig().getString("storage-system");
 
-    public void startSaveTask(){
-        new BukkitRunnable(){
+		if (Objects.requireNonNull(storageSystem).equalsIgnoreCase("mysql")) {
+			this.playerStorage = new PlayerStorageSql();
+			this.boostersStorage = new BoosterStorageSql();
+		} else {
+			this.playerStorage = new PlayerStorageYaml();
+			this.boostersStorage = new BoosterStorageYaml();
+		}
+	}
 
-            @Override
-            public void run() {
-                playerStorage.savePlayers();
-            }
-        }.runTaskTimer(this, getConfig().getInt("saving-time")* 20L, getConfig().getInt("saving-time")* 20L);
-    }
+	public void startSaveTask() {
+		new BukkitRunnable() {
 
-    public BoostersManager getBoostersManager() {
-        return boostersManager;
-    }
+			@Override
+			public void run() {
+				playerStorage.savePlayers();
+			}
+		}.runTaskTimer(this, getConfig().getInt("saving-time") * 20L, getConfig().getInt("saving-time") * 20L);
+	}
 
-    public BoostersStorage getActiveBoostersManager() {
-        return boostersStorage;
-    }
+	public BoostersManager getBoostersManager() {
+		return boostersManager;
+	}
 
-    public PlayerStorage getPlayerStorage() {
-        return playerStorage;
-    }
+	public BoostersStorage getActiveBoostersManager() {
+		return boostersStorage;
+	}
 
-    public BoosterBar getBoosterBar() {
-        return boosterBar;
-    }
+	public PlayerStorage getPlayerStorage() {
+		return playerStorage;
+	}
 
-    public Messages getMessages() {
-        return messages;
-    }
+	public BoosterBar getBoosterBar() {
+		return boosterBar;
+	}
+
+	public Messages getMessages() {
+		return messages;
+	}
 }
