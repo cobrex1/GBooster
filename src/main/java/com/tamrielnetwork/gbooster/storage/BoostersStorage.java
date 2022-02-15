bf2eb0faab27d9944107644fc7d5aa4cf1c0ba3a
TamrielNetwork <contact@tamriel.me>
2022-02-15 19:08:56 +0100

    Update Copyright

--- src/main/java/com/tamrielnetwork/gbooster/storage/BoostersStorage.java
+++ src/main/java/com/tamrielnetwork/gbooster/storage/BoostersStorage.java
@@ -1,20 +1,21 @@
 /*
- GBooster is a Spigot Plugin providing Global Boosters.
- Copyright (C) 2022  Leopold Meinel
-
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with this program. If not, see https://github.com/TamrielNetwork/GBooster/blob/main/LICENSE.
+ * GBooster is a Spigot Plugin providing Global Boosters for Jobs McMMO and Minecraft.
+ * Copyright © 2022 Leopold Meinel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see https://github.com/TamrielNetwork/GBooster/blob/main/LICENSE
  */
+
 package com.tamrielnetwork.gbooster.storage;
 
 import com.google.common.collect.ArrayListMultimap;
